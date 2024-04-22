2f167e2a3d1d69772cc440c6dc0d9b273f0463c0
human2402 <65188741+human2402@users.noreply.github.com>
2024-04-22 22:34:23 +0900

    Update task-rewrite.py

--- task-rewrite.py
+++ task-rewrite.py
@@ -84,9 +84,59 @@ def godPleaseWork(c, A, B):
                 usedCellsCount += 1
     if usedCellsCount == supposedCellsCount:
         isSupported = True
-    print("used cells: ", usedCellsCount, "m+n-1: ", usedCellsCount)
+    print("used cells:", usedCellsCount, "| m+n-1:", usedCellsCount)
+    
+    
     if isSupported:
-        print ("continue pls")
+        priceF = 0
+        for i in range(len(allocation)):
+            for j in range(len(allocation[i])):
+                if allocation[i][j] != 'X':
+                    priceF += allocation[i][j]*c[i][j]
+                    
+        print ("F:", priceF)
+
+        # Calculate potentials
+        pot_A = ["X"] * len(A)
+        pot_B = ["X"] * len(B)
+        pot_A[0] = 0  # Arbitrarily set one potential to zero
+
+        # i, j = 0, 0
+        # while i <= len(pot_A)-1 and j <= len(pot_B)-1:
+        #     if allocation[i][j]  != "X":
+        #         pot_B[j] = c[i][j] - pot_A[i]
+        #     j+=1
+        #     if j==len(pot_B):
+                
+        #         i+=1
+        #     print (i, j, pot_A, pot_B)
+            
+            
+        #     if j>len(pot_B): break
+
+        for i in range(len(pot_A)):
+            for j in range(len(pot_B)):
+                if allocation[i][j]  != "X":
+                    if pot_A[i] != "X":
+                        pot_B[j] = c[i][j] - pot_A[i]
+                    else:
+                        pot_A[i] = c[i][j] - pot_B[j]
+        
+                print (i, j, pot_A, pot_B)
+            
+            
+        #     if j>len(pot_B): break
+
+        result_dict["potentials"] = {"A": pot_A, "B": pot_B}
+
+        # Print potentials
+        print("Potentials for A:", pot_A)
+        print("Potentials for B:", pot_B)
+
+
+
+
+
     else:
         print("plan failed")
         
